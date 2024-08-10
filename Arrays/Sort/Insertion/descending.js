9892fc15a67ed2f0e385fe8fc636431fe44ea880
Mona Lisa <bhamannyrat@gmail.com>
2024-08-10 20:52:38 +0530

    insertion sorting done

--- Arrays/Sort/Insertion/descending.js
+++ Arrays/Sort/Insertion/descending.js
@@ -1,1 +1,11 @@
 let array = [9, 21, 41, 43,  51,  49, 5, 54, 45, 6, 7, 10];
+
+for (let i = 1; i < array.length; i++) {
+    let temp = array[i];
+    for (let j = i-1; j >= 0 && temp > array[j]; j--) {
+        store = array[j];
+        array[j] = temp;
+        array[j+1] = store;
+    }
+}
+console.log(array);
